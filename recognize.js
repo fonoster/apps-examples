7783d2f00646ae3c4bfd5f20f042d3a67750a5f3
Pedro Sanders <sanderspedro@gmail.com>
2016-06-13 17:13:20 -0400

    Create recognize.js

---
+++ recognize.js
@@ -0,0 +1,5 @@
+// This is just a draft of the functionality. 
+// It may change in the near future
+say("Prepare to recognize");
+var txt = recognize("en-US_NarrowbandModel");
+say("Did you say " + txt + "?");
