e3b745be7bf1faeadd3f60b3aa16e9701745a597
Pedro Sanders <sanderspedro@gmail.com>
2017-03-26 15:36:49 -0400

    Update fruits-basket.js

--- fruits-basket.js
+++ fruits-basket.js
@@ -3,7 +3,7 @@ loadJS("fn:http.js");
 var fruits;
 
 // Get fruits from json resource
-$http.get('https://raw.githubusercontent.com/fonoster/apps-examples/master/fruits.json')
+$http.get('https://raw.githubusercontent.com/psanders/fnjsexamples/master/fruits-basket.js')
 .then(function(result) {
 	fruits = JSON.parse(result.body);
 });
